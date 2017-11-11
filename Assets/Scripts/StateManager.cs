954ee1471c389d5666c6384f1114a1c007ccfbf7
shiftay <shroebuck@gmail.com>
2017-11-11 14:44:37 -0500

    Basic functionality to State Machine.

---
+++ Assets/Scripts/StateManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameStates { MENU, PAUSE, PLAY, SETTINGS }
+
+public class StateManager : MonoBehaviour {
+
+	private GameStates currentState;
+	private GameStates lastState;
+	public GameStates STATE { get { return currentState; }
+							  set { lastState = currentState; 
+								  	currentState = value;
+									ChangeState();		}}
+	private Transform[] states;
+	public static StateManager instance;
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+		states = new Transform[transform.childCount];
+		currentState = GameStates.MENU;
+		
+		for(int i = 0; i < transform.childCount; i++) {
+			states[i] = transform.GetChild(i);
+			transform.GetChild(i).gameObject.SetActive(false);
+		}
+
+		states[(int)currentState].gameObject.SetActive(true);
+	}
+
+	void ChangeState() {
+		if(lastState == GameStates.PLAY && currentState == GameStates.PAUSE){
+			// turn on pause
+			states[(int)currentState].gameObject.SetActive(true);
+		} else if (lastState == GameStates.PAUSE && currentState == GameStates.PLAY) {
+			// turn off just pause
+			states[(int)lastState].gameObject.SetActive(false);
+		} else {
+			states[(int)lastState].gameObject.SetActive(false);
+			states[(int)currentState].gameObject.SetActive(true);
+		}
+	}
+
+}
