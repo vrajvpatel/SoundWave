58a6f7ff74faa0c1fb642d7303ab93d324507ef7
shiftay <shroebuck@gmail.com>
2017-11-11 20:43:01 -0500

    The game manager will be responsible for storing most information

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
+// place holder, need more information.
+public class Settings {
+	public float sfxVolume;
+	public float musicVolume;
+	public bool turnOffAnimations;
+
+	Settings() {
+		sfxVolume = 100;
+		musicVolume = 100;
+		turnOffAnimations = false;
+	}
+}
+
+/// <summary>
+/// Used to store settings, scores, and read / write
+/// </summary>
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
+	const string SETTINGSPATH = "Assets/settings.dat";
+	const string SCORESPATH = "Assets/scores.dat";
+	private Settings gameSettings;
+	Dictionary<string,int> Scores = new Dictionary<string, int>();
+	private static GameManager instance;
+	public GameManager Instance { get { return instance; }}
+
 	void Start () {
+		instance = this;
+
+		if(!File.Exists(SETTINGSPATH)) {
+			File.Create(SETTINGSPATH);
+		} else {
+			ReadSettings();
+		}
+
+		if(!File.Exists(SCORESPATH)) {
+			File.Create(SCORESPATH);
+		} else {
+			ReadScores();
+		}
+	}
+
+
+	// READ THE FILE
+	private void ReadSettings() {
+
+	}
+
+	private void ReadScores() {
+
+	}
+
+	// WRITE THE FILE
+	private void WriteSettings() {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private void WriteScores() {
+
+	}
+
+
+
+	/// <summary>
+	/// Callback sent to all game objects before the application is quit.
+	/// </summary>
+	void OnApplicationQuit() {
+		WriteSettings();
+		WriteScores();
 	}
 }
